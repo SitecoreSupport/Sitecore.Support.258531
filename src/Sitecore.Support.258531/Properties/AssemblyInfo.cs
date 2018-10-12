3a5278b5b20aee2f16d227d09490bdd0c70ab903
DK\IEKA <ieka@sitecore.net>
2018-10-12 09:57:05 +0300

    Fixing the reason that causes "InvalidOperationException: renderingItemId".

---
+++ src/Sitecore.Support.258531/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.258531")]
+[assembly: AssemblyProduct("Sitecore.Support.258531")]
+[assembly: ComVisible(false)]
